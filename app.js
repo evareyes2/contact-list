37678727aa11af6e1338abeb063f74a5a15ac417
Eva Reyes <macbook@MacBook-Air-de-MacBook.local>
2023-09-12 17:48:20 -0500

    final-2

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
-function Contacto(nombre, telefono) {
-    this.nombre = nombre;
+function Contacto(id, nombres, apellidos, telefono, ubicaciones) {
+    this.id = id;
+    this.nombres = nombres;
+    this.apellidos = apellidos;
     this.telefono = telefono;
+    this.ubicaciones = ubicaciones;
   }
   
 
   const listaDeContactos = [
-    new Contacto("Eva Reyes", "111 111111"),
-    new Contacto("José Ignacio", "222 222222"),
-    new Contacto("María Vargas", "333 333333"),
-    new Contacto("Diego Oviedo", "444 444444"),
-    new Contacto("Juan Andrés", "555 555555"),
+    new Contacto(1, "Eva", "Reyes", "111 111111",[{ciudad: "Bogotá", direccion: "calle 1"}]),
+    new Contacto(2, "José", "Barraquer", "222 222222", [{ciudad: "Bogotá", direccion: "calle 2"}]),
+    new Contacto(3, "María", "Vargas", "333 333333", [{ciudad: "Cartagena", direccion: "calle 3"}]),
+    new Contacto(4, "Diego", "Oviedo", "444 444444", [{ciudad: "Cali", direccion: "calle 4"}]),
+    new Contacto(5, "Juan", "Andrés", "555 555555", [{ciudad: "Medellín", direccion: "calle 5"}]),
   ];
   
 
-  function agregarContacto(nombre, telefono) {
-    const nuevoContacto = new Contacto(nombre, telefono);
+  function agregarContacto(id, nombres, apellidos, telefono, ubicaciones) {
+    const nuevoContacto = new Contacto(id, nombres, apellidos, telefono, ubicaciones);
     listaDeContactos.push(nuevoContacto);
   }
   
 
-  function borrarContacto(nombre) {
-    const indice = listaDeContactos.findIndex((contacto) => contacto.nombre === nombre);
+  function borrarContacto(id) {
+    const indice = listaDeContactos.findIndex((contacto) => contacto.id === id);
     if (indice !== -1) {
       listaDeContactos.splice(indice, 1);
     }
@@ -29,10 +32,13 @@ function Contacto(nombre, telefono) {
   
   function imprimirContactos() {
     listaDeContactos.forEach((contacto) => {
-      console.log(`Nombre: ${contacto.nombre}, Teléfono: ${contacto.telefono}`);
+      console.log(`ID: ${contacto.id}, Nombres: ${contacto.nombres}, Apellidos: ${contacto.apellidos}, Teléfono: ${contacto.telefono}`);
+      contacto.ubicaciones.forEach((ubicacion) => {
+        console.log(`  Ciudad: ${ubicacion.ciudad}, Dirección: ${ubicacion.dirección}`);
+      });
     });
   }
 
-  agregarContacto("Pedro Andrade", "666 666666");
-  borrarContacto("Juan Andrés");
+  agregarContacto(6, "Pedro", "Andrade", "666 666666", [{ ciudad: "Barranquilla", dirección: "Dirección6" }]);
+  borrarContacto(5);
   imprimirContactos();
